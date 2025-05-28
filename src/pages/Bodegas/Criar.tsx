93dbbaabfd19487aa400973d17d6356cc4ad615c
Yuri R <yurir332@gmail.com>
2025-05-28 11:45:53 -0300

    ajustes bodegas

--- src/pages/Bodegas/Criar.tsx
+++ src/pages/Bodegas/Criar.tsx
@@ -107,7 +107,7 @@ const CriarBodega = () => {
                 />
               </div>
 
-              <div className="space-y-2 md:col-span-2">
+              {/* <div className="space-y-2 md:col-span-2">
                 <Label htmlFor="bodega_descripcion">Descripción</Label>
                 <textarea
                   id="bodega_descripcion"
@@ -115,9 +115,8 @@ const CriarBodega = () => {
                   value={formData.bodega_descripcion}
                   onChange={handleChange}
                   className="w-full border rounded-md px-3 py-2 min-h-[100px]"
-                  required
                 />
-              </div>
+              </div> */}
 
               <div className="space-y-2">
                 <Label htmlFor="bodega_altura">Altura (m)</Label>
