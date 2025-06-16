53e3698fc77017a03d56604a7390387f5a0b98e7
Yhordan Miller <yhordanmiller@feetcolombia.com>
2025-06-16 10:06:05 -0500

    feat (inventario) actualiza componente

--- src/components/inventario/inventarioProductosApi.ts
+++ src/components/inventario/inventarioProductosApi.ts
@@ -1,14 +1,18 @@
 import axios from 'axios';
 
-// const BASE_URL = 'https://stg.feetcolombia.com/rest/V1';
 const BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/rest/V1`;
 export const UrlImagen = 'https://catalogo.feetcolombia.com/media/catalog/product/';
 
 export const inventarioProductosApi = {
-  getCustomProducts: async (type?: string, brand?: string): Promise<any> => {
+  getCustomProducts: async (
+    type?: string,
+    brand?: string,
+    source?: string
+  ): Promise<any> => {
     let url = `${BASE_URL}/custom-products?`;
-    url += `type=${type ? encodeURIComponent(type) : 'M'}`;
-    if(brand) {
+    url += `source=${encodeURIComponent(source ?? 'default')}`;
+    url += `&type=${encodeURIComponent(type ?? '')}`;
+    if (brand) {
       url += `&brand=${encodeURIComponent(brand)}`;
     }
     try {
